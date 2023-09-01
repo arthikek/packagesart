4581f44e65902ed1091991394cf993d219dca192
Arthike Kandasamy <arthike36@gmail.com>
2023-09-01 17:22:09 +0200

    updated launch script

--- raspimouse/launch/combined_launch.py
+++ raspimouse/launch/combined_launch.py
@@ -16,4 +16,18 @@ def generate_launch_description():
             name='hat_node',
             output='screen'
         ),
+         Node(
+            name='rplidar_composition',
+            package='rplidar_ros',
+            executable='rplidar_composition',
+            output='screen',
+            parameters=[{
+                'serial_port': '/dev/ttyUSB0',
+                'serial_baudrate': 115200,  # A1 / A2
+                # 'serial_baudrate': 256000, # A3
+                'frame_id': 'laser',
+                'inverted': False,
+                'angle_compensate': True,
+            }],
+        ),
     ])
